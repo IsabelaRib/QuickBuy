dde99a914034a39058c6dd6103d5de47c231c3d9
Isabela Ribeiro de Oliveira <isabelarib.oliveira@gmail.com>
2023-04-14 14:05:16 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
